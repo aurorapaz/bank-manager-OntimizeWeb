4b38cbe0b151b39faf2431a872918391d20a0a3e
aurorapaz <aurorapazperez@hotmail.com>
2020-07-15 13:39:04 +0200

    Exercise_4

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -5,12 +5,18 @@ import { AuthGuardService } from 'ontimize-web-ngx';
 import { MainComponent } from './main.component';
 import { HomeModule } from './home/home.module';
 import { CustomersModule } from './customers/customers.module';
+import { EmployeesModule } from './employees/employees.module';
 
 export function loadHomeModule() {
   return HomeModule;
 }
+
 export function loadCustomersModule() {
   return CustomersModule;
+}
+
+export function loadEmployeesModule(){
+  return EmployeesModule;
 }
 
 export const routes: Routes = [
@@ -20,14 +26,9 @@ export const routes: Routes = [
     canActivate: [AuthGuardService],
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      {
-        path: 'home',
-        loadChildren: loadHomeModule
-      },
-      {
-        path: 'customers',
-        loadChildren: loadCustomersModule
-      }
+      { path: 'home', loadChildren: loadHomeModule },
+      { path: 'customers', loadChildren: loadCustomersModule },
+      { path: 'employees', loadChildren: loadEmployeesModule }
     ]
   }
 ];
