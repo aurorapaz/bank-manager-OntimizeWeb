98a8df72d54b48a6b41ccb9eb95171c15f046346
aurorapaz <aurorapazperez@hotmail.com>
2020-07-15 12:42:18 +0200

    Exercise_2

--- src/app/shared/app.menu.config.ts
+++ src/app/shared/app.menu.config.ts
@@ -1,6 +1,7 @@
 import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
-  { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
+  { id: 'home', name: 'HOME', icon: 'people', route: '/main/customers' },
+  { id: 'customers', name: 'CUSTOMERS', icon: 'home', route: '/main/home' },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
